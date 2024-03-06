774e2b6188b3c30922a54ae4aa300f2ed4a39574
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2024-03-06 05:49:36 +0300

    Add files via upload

---
+++ 2.unite 28 uygulam/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2.unite_28_uygulam
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int dongu = 1;
+            int toplam = 0;
+            while(toplam <= 1000)
+            {
+                toplam += dongu;
+                dongu += 1;
+
+            }
+            MessageBox.Show(dongu.ToString());
+        }
+    }
+}
